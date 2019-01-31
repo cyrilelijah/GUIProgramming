14d799f80c8135c120a2d06aa84739112edab239
cyrilelijah <cyrilelijahaurino@gmail.com>
2019-01-31 23:41:21 +0800

    Update Example.java

--- src/Containers/Example.java
+++ src/Containers/Example.java
@@ -16,17 +16,15 @@ import javax.swing.JPanel;
  *
  * @author Cyril
  */
-public class Example extends JFrame{//1st step extends to Jframe
-    private JPanel buttonPanel;//5th declare components
+public class Example extends JFrame{
+    private JPanel buttonPanel;
     private JButton button1;
     private JLabel label;
-    public Example(){//2nd step create a constructor
-        setSize(400, 400);//3rd step set the size of Jframe
-        setVisible(true);//4th
-        
-        buttonPanel = new JPanel();//6th instantiate
-        buttonPanel.setLayout(null);//7th set the layout
-        add(buttonPanel);//8th add to the jframe
+    public Example(){
+        setSize(400, 400);
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(null);
+        add(buttonPanel);
         
 //        buttonPanel.setBackground(Color.DARK_GRAY);
         label = new JLabel(new ImageIcon(getClass().getResource("butterfly.jpg")));
@@ -43,7 +41,7 @@ public class Example extends JFrame{//1st step extends to Jframe
     }
     
     public static void main(String[] args) {
-        new Example();
+        new Example().setVisible(true);
     }
     
     
